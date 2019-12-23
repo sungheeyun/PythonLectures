0c34b9dd77d0ba97b3f209060045c4118d233fb2
Sunghee Yun <sunyun@gmail.com>
2019-12-22 18:11:58 -0800

    Added dict exercise files

---
+++ src/dict/merge_dict.py
@@ -0,0 +1,17 @@
+
+def merge_dict(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+
+    return merged_dict
+
+
+def merge_dict1(dict1, dict2):
+    merged_dict = dict()
+    for key, value in dict1.items():
+        merged_dict[key] = value
+
+    for key, value in dict2.items():
+        merged_dict[key] = value
+
+    return merged_dict
---
+++ src/dict/print_dict.py
@@ -0,0 +1,8 @@
+def print_dict(d):
+    for key, value in d.items():
+        print(key, value)
+
+
+def print_dict1(d):
+    for key in d.keys():
+        print(key, d[key])
